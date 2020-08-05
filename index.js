9bff384f1d61408c9fdc20dbe25fb9d1cd2ef6fc
Devin Hunt <devin@hailpixel.com>
2020-08-05 20:39:14 +0100

    The server can now handle multiple data sources

--- index.js
+++ index.js
@@ -7,23 +7,33 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
-const CSV_PATH = path.resolve(__dirname, 'data.csv')
-const sheetId = `1VM2N68-aAGp8_WWvxtcEvgGf9YBwW3AzXB4Q2xEynLA` // Should prob be an enviroment var
-const sheetsUrlTemplate = docId => `https://docs.google.com/spreadsheets/d/${docId}/pub?output=csv`
+const getCsvPath = sourceKey => path.resolve(__dirname, `data-${sourceKey}.csv`)
+const getSheetsUrl = docId => `https://docs.google.com/spreadsheets/d/${docId}/pub?output=csv`
+
 let dataFetched = false
 
+const dataRoutes = [
+  ['default', `1VM2N68-aAGp8_WWvxtcEvgGf9YBwW3AzXB4Q2xEynLA`],
+  ['trend', `17L_APNOBkXcd8nsBkflxrIn_7ql2xVM2F2NNNp3DagM`],
+  ['policy', `1iZf2Y0w43WGmqgiCkpwpje4XxRIyDubW6rYKt1UWSS4`]
+]
+
 async function fetchData() {
-  const dataUri = sheetsUrlTemplate(sheetId)
-  const response = await fetch(dataUri)
-  const body = await response.text()
 
-  fs.writeFileSync(CSV_PATH, body)
+  await Promise.all(dataRoutes.map( async ([key, sheetId]) => {
+    const dataUri = getSheetsUrl(sheetId)
+    const response = await fetch(dataUri)
+    const body = await response.text()
 
-  console.log('Data updated at', new Date().toUTCString())
+    fs.writeFileSync(getCsvPath(key), body)
+    console.log(`--- Data source ${key} update at ${new Date().toUTCString()}`)  
+  }))
+
+  console.log('All sources updated at', new Date().toUTCString())
   dataFetched = true
 
-  // Check for updates every 2 minutes
-  setTimeout(() => fetchData(), 2 * 60 * 1000)
+  // Check for updates every 5 minutes
+  setTimeout(() => fetchData(), 5 * 60 * 1000)
 }
 
 // kickoff fetching
@@ -31,9 +41,9 @@ async function fetchData() {
 
 app.get('/', (req, res) => res.status(501).send('Invalid request'))
 
-app.get('/data', cors(), (req, res) => {
+app.get('/data/:source', cors(), (req, res) => {
   if(dataFetched) {
-    res.download(CSV_PATH)
+    res.download(getCsvPath(req.params.source))
   } else {
     res.status(500).send('Data not present')
   }  
